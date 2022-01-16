bcfdae73757cf4de372fb8e19acbbe4ee61ed4f6
marc-dantas <marciodantasms@hotmail.com>
2022-01-16 20:00:35 -0300

    Update lyon.py

--- lang/lyon.py
+++ lang/lyon.py
@@ -1,13 +1,19 @@
-from engine.actions import COMMANDS, PROCESSOR, INTERPRETER
+from engine.actions import COMMANDS
 import shell
 import engine.core
 import engine.util
 
+# Constants
+TABLE = engine.core.CommandTable()
+TABLE.insert_commands(COMMANDS)
+PROCESSOR = engine.core.CommandProcessor()
+INTERPRETER = engine.core.CommandInterpreter(TABLE)
+
 
 def main() -> None:
-    shell.print_header(title='Lyon', subtitle='Interactive console')
-    shell.print_hint('Type "ext" to exit')
-    shell.print_hint('Type "runfile <filename>" to run a file')
+    shell.print_header('Lyon', 'Interactive console')
+    shell.print_hint('Type "ext" to [bold]exit[/]')
+    shell.print_hint('Type "run <filename>" to [bold]run[/] a file (function)')
     shell.print_hint('See docs at <https://github.com/marc-dantas/lyon/blob/master/docs/index.md>')
     while True:
         cmd = shell.get()
